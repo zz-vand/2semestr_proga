09516d0c90679891fcf3c7e24bb80c1160ad1285
AndrewVlasov-st <130500927+zz-vand@users.noreply.github.com>
2025-02-18 19:37:09 +0300

    Update utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,1 +1,20 @@
+#include <vector>
 
+class Solution {
+public:
+    int removeDuplicates(std::vector<int>& nums) {
+        int j = 1;
+
+        for (int i = 1; i < nums.size(); i++){
+            if (nums[i] != nums[i - 1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j;
+    }
+    int plus(int a, int b){
+        return a+b;
+        }
+};
